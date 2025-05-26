357d550f15fb3ec8bf26e6b01724277d2831d4c3
VieHiroto <www.nkuc@gmail.com>
2025-05-27 05:38:16 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-
-const LINE_TOKEN = process.env.LINE_TOKEN;
+const cors = require('cors');
 
 const app = express();
+
+// CORSを全許可（必要に応じて制限も可能）
+app.use(cors());
+
 app.use(bodyParser.json());
+
+const LINE_TOKEN = process.env.LINE_TOKEN;
 
 app.post('/', async (req, res) => {
   const { userId, message } = req.body;
@@ -15,7 +20,7 @@ app.post('/', async (req, res) => {
   }
 
   try {
-    const result = await axios.post('https://api.line.me/v2/bot/message/push', {
+    await axios.post('https://api.line.me/v2/bot/message/push', {
       to: userId,
       messages: [{
         type: 'text',
